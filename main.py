37a58233c3e52e0c8eb9bd7366da4308beb178e6
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-11-22 01:23:00 +0000

    :sparkles: Format code with `black` `isort`

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import subprocess
-from PIL import Image
-from loguru import logger
+import math
 import os
+import subprocess
 from os import listdir
 from os.path import isfile, join
-import math
+
+from loguru import logger
+from PIL import Image
+
 try:
-    os.mkdir('Output')
+    os.mkdir("Output")
 except Exception as e:
     logger.error(e)
 onlyfiles = [f for f in listdir("Input") if isfile(join("Input", f))]
 
 
 def resize(image_pil, width, height):
-    '''
+    """
     Resize PIL image keeping ratio and using white background.
-    '''
+    """
     ratio_w = width / image_pil.width
     ratio_h = height / image_pil.height
     if ratio_w < ratio_h:
@@ -27,62 +29,66 @@ def resize(image_pil, width, height):
         resize_width = round(ratio_h * image_pil.width)
         resize_height = height
     image_resize = image_pil.resize(
-        (resize_width, resize_height), Image.Resampling.LANCZOS)
+        (resize_width, resize_height), Image.Resampling.LANCZOS
+    )
     # or (255, 255, 255, 255)
-    background = Image.new('RGBA', (width, height), (0, 0, 0, 0))
-    offset = (round((width - resize_width) / 2),
-              round((height - resize_height) / 2))
+    background = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    offset = (round((width - resize_width) / 2), round((height - resize_height) / 2))
     background.paste(image_resize, offset)
     return background
 
 
 def makeWebmicon(f: str):
-    b1 = os.getcwd()+'/'+f[:f.find('.')]
-    b2 = os.getcwd()+'/'+f
+    b1 = os.getcwd() + "/" + f[: f.find(".")]
+    b2 = os.getcwd() + "/" + f
     subprocess.call(
-        f'ffmpeg -i {b2} -fs 32KB -c:v libvpx-vp9 -loglevel quiet -vf scale=100:100 {b1}.webm')
+        f"ffmpeg -i {b2} -fs 32KB -c:v libvpx-vp9 -loglevel quiet -vf scale=100:100 {b1}.webm"
+    )
 
 
 def smallPicResize(image_pil):
-    salt = math.sqrt(512/(image_pil.height))
+    salt = math.sqrt(512 / (image_pil.height))
     # change this value if you think the ouput img is unacceptable
-    salt *= int(pow(salt*salt, 1/3))
-    resize_w = int(image_pil.width*salt)
-    resize_h = int(image_pil.height*salt)
-    image_resize = image_pil.resize(
-        (resize_w, resize_h), Image.Resampling.LANCZOS)
-    background = Image.new('RGBA', (512, 512), (0, 0, 0, 0))
-    offset = (round((512-resize_w)/2),
-              round((512-resize_h)/2))
+    salt *= int(pow(salt * salt, 1 / 3))
+    resize_w = int(image_pil.width * salt)
+    resize_h = int(image_pil.height * salt)
+    image_resize = image_pil.resize((resize_w, resize_h), Image.Resampling.LANCZOS)
+    background = Image.new("RGBA", (512, 512), (0, 0, 0, 0))
+    offset = (round((512 - resize_w) / 2), round((512 - resize_h) / 2))
     background.paste(image_resize, offset)
     return background
 
 
 for file in onlyfiles:
-    if file[file.find('.'):].lower() in ['.webp', '.png', '.jpg']:
-        image = Image.open(os.getcwd()+'/Input/'+file, 'r')
+    if file[file.find(".") :].lower() in [".webp", ".png", ".jpg"]:
+        image = Image.open(os.getcwd() + "/Input/" + file, "r")
         # special process for very small picture
         if image.width < 200 or 200 > image.height:
-            smallPicResize(image).save(os.getcwd()+'/Output/' +
-                                       file[:file.find('.')]+'.png', quality=100)
-            logger.info('\033[92m[small pic]\033[39m'+file)
+            smallPicResize(image).save(
+                os.getcwd() + "/Output/" + file[: file.find(".")] + ".png", quality=100
+            )
+            logger.info("\033[92m[small pic]\033[39m" + file)
         else:
-            resize(image, 512, 512).save(os.getcwd()+'/Output/' +
-                                         file[:file.find('.')]+'.png', quality=100)
+            resize(image, 512, 512).save(
+                os.getcwd() + "/Output/" + file[: file.find(".")] + ".png", quality=100
+            )
             logger.info(file)
-    elif file[file.find('.'):].lower() == '.gif':
-        image = Image.open(os.getcwd()+'/Input/'+file, 'r')
-        b1 = os.getcwd()+'/Output/'+file[:file.find('.')]
-        b2 = os.getcwd()+'/Input/'+file
+    elif file[file.find(".") :].lower() == ".gif":
+        image = Image.open(os.getcwd() + "/Input/" + file, "r")
+        b1 = os.getcwd() + "/Output/" + file[: file.find(".")]
+        b2 = os.getcwd() + "/Input/" + file
         logger.info(file)
         if image.height > image.width:
             subprocess.check_output(
-                f'ffmpeg -i {b2} -c:v libvpx-vp9 -r 30 -loglevel quiet -vf scale=-1:512 {b1}.webm')
+                f"ffmpeg -i {b2} -c:v libvpx-vp9 -r 30 -loglevel quiet -vf scale=-1:512 {b1}.webm"
+            )
         elif image.width > image.height:
             subprocess.check_output(
-                f'ffmpeg -i {b2} -c:v libvpx-vp9 -r 30 -loglevel quiet -vf scale=512:-1 {b1}.webm')
+                f"ffmpeg -i {b2} -c:v libvpx-vp9 -r 30 -loglevel quiet -vf scale=512:-1 {b1}.webm"
+            )
         else:
             subprocess.check_output(
-                f'ffmpeg -i {b2} -c:v libvpx-vp9 -r 30 -loglevel quiet -vf scale=512:512 {b1}.webm')
+                f"ffmpeg -i {b2} -c:v libvpx-vp9 -r 30 -loglevel quiet -vf scale=512:512 {b1}.webm"
+            )
     else:
-        logger.warning(f'ignore: {file}')
+        logger.warning(f"ignore: {file}")
